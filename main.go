f808801cb640773a3f80d51975dad12c11d98cc2
Алексей Троянов <troyanov.a@rt-hpc.ru>
2023-05-01 21:43:27 +0500

    Added random password generator

--- main.go
+++ main.go
@@ -12,10 +12,12 @@ import (
 	"io/ioutil"
 	"log"
 	"math/big"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -23,6 +25,8 @@ import (
 )
 
 var wg sync.WaitGroup
+
+const alphaNums = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
 
 func SHA256(hasher hash.Hash, input []byte) (hash []byte) {
 	hasher.Reset()
@@ -55,6 +59,18 @@ func GenerateAddressFromPrivKey(hex string) string {
 	return address
 }
 
+func BalanceAt(client *ethclient.Client, address string) (*big.Int, error) {
+	account := common.HexToAddress(address)
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		if err == io.EOF {
+			log.Fatalf("Check balance: %s %v\n", address, err)
+		}
+		return nil, err
+	}
+	return balance, nil
+}
+
 func WriteToFound(text string, path string) {
 	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0655)
 	if err != nil {
@@ -68,19 +84,37 @@ func WriteToFound(text string, path string) {
 	}
 }
 
+func CheckBalanceRandom(client *ethclient.Client, length int) {
+	for {
+		password := genRandomPassword(length)
+		privKey := NewPrivateKey(password)
+		address := GenerateAddressFromPrivKey(privKey)
+
+		balance, err := BalanceAt(client, address)
+
+		if err != nil {
+			log.Printf("Random: %v", err)
+			continue
+		}
+
+		if balance.Cmp(big.NewInt(0)) != 0 {
+			data := fmt.Sprintf("%s:%s:%s %s\n", password, privKey, address, balance.String())
+			WriteToFound(data, "found.txt")
+		}
+		fmt.Println(password, privKey, address, balance)
+
+	}
+}
+
 func CheckBalance(passwords chan string, client *ethclient.Client) {
 	for password := range passwords {
 		privKey := NewPrivateKey(password)
 		address := GenerateAddressFromPrivKey(privKey)
 		creds := fmt.Sprintf("%s:%s", privKey, address)
 
-		account := common.HexToAddress(address)
-		balance, err := client.BalanceAt(context.Background(), account, nil)
+		balance, err := BalanceAt(client, address)
 
 		if err != nil {
-			if err == io.EOF {
-				log.Fatalf("Check balance: %s %v\n", creds, err)
-			}
 			log.Printf("Check balance: %s %v\n", creds, err)
 			continue
 		}
@@ -104,12 +138,45 @@ func GetPasswordList(path string) ([]string, error) {
 	return passwords, nil
 }
 
+func genRandomPassword(length int) (password string) {
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < length; i++ {
+		symbol := alphaNums[rand.Intn(len(alphaNums)-1)]
+		password += string(symbol)
+	}
+	return
+}
+
+func GenRandomPasswords(length int, num int) (results []string) {
+	for i := 0; i < num; i++ {
+		password := genRandomPassword(length)
+		results = append(results, password)
+	}
+	return
+}
+
 func main() {
 	passFile := flag.String("i", "passwords.txt", "Password list")
 	threads := flag.Uint("t", 4, "Number of threads")
-	server := flag.String("s", "95.179.253.164", "Ethereum rpc server")
+	server := flag.String("s", "154.48.217.250", "Ethereum rpc server")
 	port := flag.Int("p", 8545, "Ethereum rpc port")
+	random := flag.Bool("r", false, "Generate random passwords")
+	length := flag.Int("l", 8, "Password length")
 	flag.Parse()
+
+	client, err := ethclient.Dial("http://" + *server + ":" + strconv.Itoa(*port))
+	if err != nil {
+		log.Fatalf("Client: %s\n", err)
+	}
+	defer client.Close()
+
+	if *random {
+		for i := 0; i < int(*threads); i++ {
+			wg.Add(1)
+			go CheckBalanceRandom(client, *length)
+		}
+		wg.Wait()
+	}
 
 	passList, err := GetPasswordList(*passFile)
 	if err != nil {
@@ -117,12 +184,6 @@ func main() {
 	}
 
 	jobs := make(chan string)
-
-	client, err := ethclient.Dial("http://" + *server + ":" + strconv.Itoa(*port))
-	if err != nil {
-		log.Fatalf("Client: %s\n", err)
-	}
-	defer client.Close()
 
 	for i := 0; i < int(*threads); i++ {
 		wg.Add(1)
